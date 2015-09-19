107e6710c0d4ba22261f9ce828241243dc857836
Bingfei <tonycao96@gmail.com>
2015-09-19 16:12:22 -0400

    index

---
+++ index.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('#userModal').on('show.bs.modal', function(event) {
+    var button = $(event.relatedTarget);
+    var type = button.data('type');
+    var modal = $(this);
+    modal.find('.modal-title').text(type);
+    if (type === 'Sign up!') {
+      modal.find('button').on('click', function() { signup(); });
+    }
+    else {
+      modal.find('button').on('click', function() { login(); });
+    }
+    console.log(modal.find('button'));
+  });
+
+  if (Parse.User.current()) {
+    $('#login').hide();
+    $('#signup').hide();
+  } else {
+    $('#logout').hide();
+  }
+});
