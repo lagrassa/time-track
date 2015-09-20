6142f96269596206fa9fd834ed50c5d36dd38145
Alexandra L LaGrassa <lagrassa@mit.edu>
2015-09-19 22:52:08 -0400

    pie chart gets correct data

--- punt.js
+++ punt.js
@@ -8,14 +8,30 @@ function drawChartGivenData(data) {
       google.load('visualization', '1.0', {'packages':['corechart']});
 
       // Set a callback to run when the Google Visualization API is loaded.
-      google.setOnLoadCallback(drawChart);
-
+      google.setOnLoadCallback(getDataAndDrawChart);
+      var timeTableID = "LG7RB4vLd9";
+      var SampleTimeTable = Parse.Object.extend("sampleTimeTable");
+      var query = new Parse.Query("sampleTimeTable");
+      query.get(timeTableID, {
+          success: function(object) {
+               timeTable = object;
+               var puntTime = timeTable.get("punting");
+               var toolTime = timeTable.get("tooling");
+               drawChart(puntTime, toolTime);
+       },
+      error: function(object, error) {
+      // error is an instance of Parse.Error.
+          console.log(error);
+      }
+     });
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
       // draws it.
-      function drawChart() {
-          var hoursPunted = 6;
-          var hoursTooled = 6;
+
+      function drawChart(hoursPunted, hoursTooled) {
+         console.log("DRAWCHART")
+         console.log(hoursPunted);
+         console.log(hoursTooled); 
         // Create the data table.
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Activity');
