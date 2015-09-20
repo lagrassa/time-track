5fbc48a88377d590b9e42a0e6b0c62c8051cd39c
Bingfei <tonycao96@gmail.com>
2015-09-19 23:48:37 -0400

    fixes

--- punt.js
+++ punt.js
@@ -1,54 +1,37 @@
+var data = 5;
 drawChartGivenData(5);
 
 
 function drawChartGivenData(data) {
 
-  // Load the Visualization API and the piechart package.
-  google.load('visualization', '1.0', {'packages':['corechart']});
+    // Load the Visualization API and the piechart package.
+    google.load('visualization', '1.0', {'packages':['corechart']});
 
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.setOnLoadCallback(getDataAndDrawChart);
-      var timeTableID = "LG7RB4vLd9";
-      var SampleTimeTable = Parse.Object.extend("sampleTimeTable");
-      var query = new Parse.Query("sampleTimeTable");
-      query.get(timeTableID, {
-          success: function(object) {
-               timeTable = object;
-               var puntTime = timeTable.get("punting");
-               var toolTime = timeTable.get("tooling");
-               console.log(puntTime);
-               console.log(toolTime);
-               drawChart(puntTime, toolTime);
-       },
-      error: function(object, error) {
-      // error is an instance of Parse.Error.
-          console.log(error);
-      }
-     });
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.setOnLoadCallback(drawChart);
 
-      function drawChart(hoursPunted, hoursTooled) {
-         console.log("DRAWCHAR WAS CALLED")
-         console.log(hoursPunted);
-         console.log(hoursTooled); 
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Activity');
-        data.addColumn('number', 'Time spent');
-        data.addRows([
-          ['Punting', hoursPunted],
-          ['Tooling', hoursTooled],
-        ]);
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart() {
+        var hoursPunted = 6;
+        var hoursTooled = 6;
+      // Create the data table.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Activity');
+      data.addColumn('number', 'Time spent');
+      data.addRows([
+        ['Punting', hoursPunted],
+        ['Tooling', hoursTooled],
+      ]);
 
-    // Set chart options
-    var options = {'title':'Punt/Tool Distribution',
-                   'width':800,
-                   'height':600};
+      // Set chart options
+      var options = {'title':'Punt/Tool Distribution',
+                     'width':800,
+                     'height':600};
 
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(document.getElementById('puntchart'));
-    chart.draw(data, options);
-  }
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(document.getElementById('puntchart'));
+      chart.draw(data, options);
+    }
 }
