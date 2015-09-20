5fa9e3e4af6888346f80c24cd5b55745de9617a4
Bingfei <tonycao96@gmail.com>
2015-09-20 03:55:05 -0400

    manual input

--- classes.js
+++ classes.js
@@ -20,7 +20,11 @@ function fillTable(user) {
       var button = '<td><button type="button" class="btn btn-info record" id="r' + course + '">Record</button></td>';
       if (!clicked[course]) { clicked[course] = false; }
       else { button = '<td><button type="button" class="btn btn-info record active" id="r' + course + '">Stop</button></td>'; }
-      $('tbody').append('<tr style="font-size: 35px;" class=' + coloring[counter] + '><td>' + course.replace('_', '.') + '</td><td>' + classes[course] + '</td>' + button);
+      var input = '<td style="width:260px"><div class="input-group col-xs-10 col-xs-offset-1"><input type="text" class="form-control" style="margin-top: 12px; font-size: 15px;"' +
+                  ' placeholder="How many hours?" id="i' + course + '"><span class="input-group-btn">' +
+                  '<button class="btn btn-default manual" style="margin-top: 12px;" type="button" id="a'+
+                  course + '">Add!</button></span></div></td>';
+      $('tbody').append('<tr style="font-size: 35px;" class=' + coloring[counter] + '><td>' + course.replace('_', '.') + '</td><td>' + classes[course] + '</td>' + button + input);
       counter = (counter + 1) % 3;
     }
   }
@@ -54,6 +58,7 @@ function drawChart(user) {
 }
 
 function setButtons() {
+  $('td').css('text-align', 'center');
   $('.record').each(function() {
     $(this).css('outline', 'none');
     $(this).click(function() {
@@ -62,22 +67,32 @@ function setButtons() {
       var name = $(this).attr('id').substring(1);
       if (clicked[name]) {
         var elapsedHours = ((new Date().getTime()) - classTimes[name])/1000/3600;
-        console.log(elapsedHours);
         clicked[name] = false;
-        var user = Parse.User.current();
-        var classes = user.get('classes');
-        classes[name] = parseFloat((classes[name] + elapsedHours).toFixed(2));
-        user.set(classes);
-        user.save(null, {
-          success: function(user) {
-            fillTable(user);
-            drawChart(user);
-          }
-        });
+        updateUser(name, elapsedHours);
       } else {
         classTimes[name] = new Date().getTime();
         clicked[name] = true;
       }
     });
+  });
+  $('.manual').each(function() {
+    $(this).click(function() {
+      var name = $(this).attr('id').substring(1);
+      var value = $('#i' + name).val();
+      updateUser(name, parseFloat(value));
+    });
+  });
+}
+
+function updateUser(course, valueToAdd) {
+  var user = Parse.User.current();
+  var classes = user.get('classes');
+  classes[course] = parseFloat((classes[course] + valueToAdd).toFixed(2));
+  user.set(classes);
+  user.save(null, {
+    success: function(user) {
+      fillTable(user);
+      drawChart(user);
+    }
   });
 }
