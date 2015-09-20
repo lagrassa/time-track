1226f7d12bab6f668f3ee9700bb0fccd8ec75b67
Alexandra L LaGrassa <lagrassa@mit.edu>
2015-09-20 00:07:33 -0400

    fixed punt/tool chart

--- punt.js
+++ punt.js
@@ -8,30 +8,15 @@ function drawChartGivenData(data) {
       google.load('visualization', '1.0', {'packages':['corechart']});
 
       // Set a callback to run when the Google Visualization API is loaded.
-      google.setOnLoadCallback(getDataAndDrawChart);
-      var timeTableID = "LG7RB4vLd9";
-      var SampleTimeTable = Parse.Object.extend("sampleTimeTable");
-      var query = new Parse.Query("sampleTimeTable");
-      query.get(timeTableID, {
-          success: function(object) {
-               timeTable = object;
-               var puntTime = timeTable.get("punting");
-               var toolTime = timeTable.get("tooling");
-               drawChart(puntTime, toolTime);
-       },
-      error: function(object, error) {
-      // error is an instance of Parse.Error.
-          console.log(error);
-      }
-     });
+      google.setOnLoadCallback(drawChart);
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
       // draws it.
 
       function drawChart(hoursPunted, hoursTooled) {
-         console.log("DRAWCHART")
-         console.log(hoursPunted);
-         console.log(hoursTooled); 
+         u = Parse.User.current();
+         var hoursPunted = u.get("punting"); 
+         var hoursTooled = u.get("tooling");
         // Create the data table.
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Activity');
@@ -51,3 +36,4 @@ function drawChartGivenData(data) {
         chart.draw(data, options);
       }
 }
+
